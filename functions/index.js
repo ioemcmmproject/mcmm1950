cd16fedd2cb2d994b7e61c77c9d20fcca52313bc
Michael Hanscom <elderhan@umich.edu>
2017-11-27 18:17:40 -0500

    updating index js

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,22 @@
 const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
-//Change
 var express = require('express');
-//
 const engines = require('consolidate');
 
-
-//Change
 var firebaseApp = firebase.initializeApp(
     functions.config().firebase
-);
-//
-
+); 
 
 function getvalue() {
     const ref = firebaseApp.database().ref();
     return ref.once('value').then(snap => snap.val());
 }
 
-//CHANGES
 var path = require('path');
 var app = express();
-//
 
-//CHANGES
+
 app.use(express.static(path.join(__dirname, '/../public')));
-//
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
@@ -47,12 +38,12 @@ app.get('/value.json', (request, response) => {
 
 app.get('/custom', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    response.render('custom', {title: 'Custom Route Generation' });
+    response.render('custom');
 });
 
-app.get('/maps', (request, response) => {
+app.get('/planned', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    response.render('maps');
+    response.render('planned');
 });
 
 
